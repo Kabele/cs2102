bfbe100afc4ac0a2d1b04711f81e0645cbacac4a
Allwin Panikulam <allwinpb@gmail.com>
2015-03-14 16:12:00 +0800

    Partialized index.php

---
+++ views/partials/styles.php
@@ -0,0 +1,4 @@
+<!-- Bootstrap -->
+<link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="/bower_components/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+<link href="/css/style.css" rel="stylesheet">
