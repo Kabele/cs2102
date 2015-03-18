2cacddbb1f30b6f41e0a9fb9a731ee008dcba8ba
Viraj <virajhk@gmail.com>
2015-03-19 04:45:10 +0800

    Improved index page

--- views/index.php
+++ views/index.php
@@ -13,7 +13,7 @@
       height: 550px; width: 100%; background-size: cover; background-repeat: no-repeat;">
 
       <div class="col-sm-5">
-        <div id="rectangle">
+        <div id="rectangle" class="bigRectangle">
           <div class="flightSearchBox">
             <hr style="height: 3px; visibility:hidden;" />
             <h3 class="centerAlign">Search for Flights!</h3>
@@ -48,7 +48,7 @@
             <select id="noofchild" style="margin-left: 40px;"></select>
             <br>
             <br>
-            <button style="margin-left: 20px;" class="searchButton" type="button">Search</button>
+            <button onclick="document.location.href='./flights.php'" style="margin-left: 20px;" class="searchButton" type="button">Search</button>
           </div>
         </div>
       </div>
@@ -60,8 +60,87 @@
 
   <div class="row">
   <div class="col-sm-4">
-    
+    <div id="rectangle" class="smallRectangle">
+      <h3 class="centerAlign">Last Added Flight</h3>
+    </div>
+    <div id="rectangle" class="mediumRectangle">
+      <li style="list-style-type: none;">
+        <img class="col-sm-4" src="./images/thumbnail.png">
+        <h4>Flight Name</h4>
+        <h5 style="display: inline">From</h5> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <h5 style="display: inline">To</h5>
+        <p>Price</p>
+      </li>
+      <li style="list-style-type: none;">
+        <img class="col-sm-4" src="./images/thumbnail.png">
+        <h4>Flight Name</h4>
+        <h5 style="display: inline">From</h5> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <h5 style="display: inline">To</h5>
+        <p>Price</p>
+      </li>
+      <li class="last" style="list-style-type: none;">
+        <img class="col-sm-4" src="./images/thumbnail.png">
+        <h4>Flight Name</h4>
+        <h5 style="display: inline">From</h5> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <h5 style="display: inline">To</h5>
+        <p>Price</p>
+      </li>
+    </div>
   </div>
+  <div class="col-sm-4">
+    <div id="rectangle" class="smallRectangle">
+      <h3 class="centerAlign">Recently Sold Flight</h3>
+    </div>
+    <div id="rectangle" class="mediumRectangle">
+    <li style="list-style-type: none;">
+        <img class="col-sm-4" src="./images/thumbnail.png">
+        <h4>Flight Name</h4>
+        <h5 style="display: inline">From</h5> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <h5 style="display: inline">To</h5>
+        <p>Price</p>
+      </li>
+      <li style="list-style-type: none;">
+        <img class="col-sm-4" src="./images/thumbnail.png">
+        <h4>Flight Name</h4>
+        <h5 style="display: inline">From</h5> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <h5 style="display: inline">To</h5>
+        <p>Price</p>
+      </li>
+      <li class="last" style="list-style-type: none;">
+        <img class="col-sm-4" src="./images/thumbnail.png">
+        <h4>Flight Name</h4>
+        <h5 style="display: inline">From</h5> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <h5 style="display: inline">To</h5>
+        <p>Price</p>
+      </li> 
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div id="rectangle" class="smallRectangle">
+      <h3 class="centerAlign">Quick Links</h3>
+    </div>
+    <div id="rectangle" class="mediumRectangle">
+    <hr style="height: 1px; visibility:hidden;" />
+    <li style="list-style-type: none;">
+      <h4>Link 1</h4>
+    </li>
+    <li style="list-style-type: none;">
+      <h4>Link 1</h4>
+    </li>
+    <li style="list-style-type: none;">
+      <h4>Link 1</h4>
+    </li>
+    <li style="list-style-type: none;">
+      <h4>Link 1</h4>
+    </li>
+    <li style="list-style-type: none;">
+      <h4>Link 1</h4>
+    </li>
+    <li class="last" style="list-style-type: none;">
+      <h4>Link 1</h4>
+    </li>
+    </div>
+  </div>  
   </div>
 
   <script src="./js/custom.js"></script>
