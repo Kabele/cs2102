038b9655a9d306f6368b5969078545e432633e28
Allwin Panikulam <allwinpb@gmail.com>
2015-03-22 13:32:35 +0800

    Users # Bookings

--- views/flights.php
+++ views/flights.php
@@ -16,34 +16,33 @@
           <li class="list-group-item">
             <div class="row">
               <div class="col-xs-8">
-                <h5>
-                  <img src="http://img2.wikia.nocookie.net/__cb20100506081728/logopedia/images/f/fa/Singapore_Airlines.svg" alt="" class="logo">
-                  <span class="lead">Singapore Airlines Flight MI467</span>
-                  <p>
-                    <ul>
-                      <li>Departing from <strong>Changi Airport</strong> on <strong>March 31st, 2015</strong> at <strong>08:00 AM</strong></li>
-                      <li>Arriving at <strong>Changi Airport</strong> on <strong>March 31st, 2015</strong> at <strong>08:00 AM</strong></li>
-                      <li>Ticket Price: <strong>S$369</strong></li>
-                      <li>Total Capacity of <strong>300</strong> passengers (<strong>198</strong> booked so far)</li>
-                      <li>Last Edited by <em>allwin@discountair.com</em></li>
-                    </ul>
-                  </p>
-                </h5>
-              </div>
-              <div class="col-xs-4 text-right">
-                <button class="btn btn-default">
-                  Edit
-                </button>
-                <button class="btn btn-danger">
-                  Delete
-                </button>
-              </div>
+                <img src="http://img2.wikia.nocookie.net/__cb20100506081728/logopedia/images/f/fa/Singapore_Airlines.svg" alt="" class="logo">
+                <span class="lead">Singapore Airlines Flight MI467</span>
+                <p>
+                  <ul>
+                    <li>Departing from <strong>Changi Airport</strong> on <strong>March 31st, 2015</strong> at <strong>08:00 AM</strong></li>
+                    <li>Arriving at <strong>Changi Airport</strong> on <strong>March 31st, 2015</strong> at <strong>08:00 AM</strong></li>
+                    <li>Ticket Price: <strong>S$369</strong></li>
+                    <li>Total Capacity of <strong>300</strong> passengers (<strong>198</strong> booked so far)</li>
+                    <li>Last Edited by <em>allwin@discountair.com</em></li>
+                  </ul>
+                </p>
+              </h5>
             </div>
-          </li>
-        </ol>
-        <a href="/flights/create.php" class="btn btn-primary">
-          Create New Flight
-        </a>
-      </div>
+            <div class="col-xs-4 text-right">
+              <button class="btn btn-default">
+                Edit
+              </button>
+              <button class="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          </div>
+        </li>
+      </ol>
+      <a href="/flights/create.php" class="btn btn-primary">
+        Create New Flight
+      </a>
     </div>
   </div>
+</div>
---
+++ views/users/bookings.php
@@ -0,0 +1,55 @@
+<?php $path = $_SERVER['DOCUMENT_ROOT'] . "/"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Discount Airlines - Create Account</title>
+	<?php include( $path . "partials/meta.php" ); ?>
+	<?php include( $path . "partials/styles.php" ); ?>
+	<?php include( $path . "partials/scripts.php" ); ?>
+</head>
+<body>
+	<?php include( $path . "partials/navbar.php" ); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 col-xs-12">
+				<ul class="nav nav-pills nav-stacked">
+					<li><a href="profile.php">Profile</a></li>
+					<li class="active"><a href="bookings.php">Bookings</a></li>
+					<li><a href="passengers.php">Passengers</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-8 col-xs-12">
+				<ul class="list-group">
+					<li class="list-group-item">
+						<div class="row">
+							<div class="col-xs-8">
+								<img src="http://img2.wikia.nocookie.net/__cb20100506081728/logopedia/images/f/fa/Singapore_Airlines.svg" alt="" class="logo">
+								<span class="lead">Singapore Airlines Flight MI467</span>
+								<span class="badge">Confirmed</span>
+								<p>
+									<ul>
+										<li>Departing from <strong>Changi Airport</strong> on <strong>March 31st, 2015</strong> at <strong>08:00 AM</strong></li>
+										<li>Arriving at <strong>Changi Airport</strong> on <strong>March 31st, 2015</strong> at <strong>08:00 AM</strong></li>
+										<li>Total Cost: <strong>S$729</strong></li>
+										<li>Weight Limit per Passenger: <strong>20 KG / 7 KG</strong></li>
+										<li>
+											Passengers
+											<ul class="passengers">
+												<li>Ross Everyman</li>
+												<li>Bob Everyman</li>
+											</ul>
+										</li>
+									</ul>
+								</p>
+							</div>
+							<div class="col-xs-4">
+								<a href="" class="btn btn-danger pull-right">Cancel</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
