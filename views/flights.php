c5184d257fe25c0f40be45f5c6064050f65cae05
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-03-22 21:35:57 +0800

    Reads flight details from database

--- views/flights.php
+++ views/flights.php
@@ -39,8 +39,57 @@
             </div>
           </div>
         </li>
+
+        <?php 
+
+        require_once $path.'php/connect.php';
+
+
+        $sql = "SELECT f.flight_id, f.departure, f.arrival, f.departure_date, f.arrival_date, f.price, f.airline_code, f.passenger_limit, f.status_changed_by, a.logo, a.name
+            FROM flight f, airline a WHERE f.airline_code = a.airline_code;";
+
+          $res = $db->query($sql);
+
+          while ($row = mysqli_fetch_assoc($res)) {
+
+              echo '<li class="list-group-item">';
+            echo '<div class="row">';
+              echo '<div class="col-xs-8">';
+                echo '<img src="'.$row['logo'].'" alt="" class="logo">';
+                echo '<span class="lead">'.$row['airline_code'].'</span>';
+                echo '<p>';
+                  echo '<ul>';
+                    echo '<li>Departing from <strong>'.$row['departure'].'</strong> on <strong>'.$row['departure_date'].'</strong> at <strong>08:00 AM</strong></li>';
+                    echo '<li>Arriving at <strong>'.$row['arrival'].'</strong> on <strong>'.$row['arrival_date'].'</strong> at <strong>08:00 AM</strong></li>';
+                    echo '<li>Ticket Price: <strong>'.$row['price'].'</strong></li>';
+                    echo '<li>Total Capacity of <strong>'.$row['passenger_limit'].'</strong> passengers (<strong>198</strong> booked so far)</li>';
+                    echo '<li>Last Edited by <em>'.$row['status_changed_by'].'</em></li>';
+                  echo '</ul>';
+                echo '</p>';
+              echo '</h5>';
+            echo '</div>';
+            echo '<div class="col-xs-4 text-right">';
+              echo '<button class="btn btn-default">';
+                echo 'Edit';
+              echo '</button>';
+              echo '<button class="btn btn-danger">';
+                echo 'Delete';
+              echo '</button>';
+            echo '</div>';
+          echo '</div>';
+        echo '</li>';
+
+
+          }
+
+
+
+
+
+        ?>        
+
       </ol>
-      <a href="/flights/create.php" class="btn btn-primary">
+      <a href="/views/flights/create.php" class="btn btn-primary">
         Create New Flight
       </a>
     </div>
