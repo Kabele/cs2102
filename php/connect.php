dc4f6f7397d49b3fc7fe2bc8cca3621dd1d2d906
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-03-26 22:23:27 +0800

    Change DB from local to remote

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,16 @@
 <?php // config.php basically contains these 4 constants
-define("db_host", "localhost");
+// define("db_host", "localhost");
+// define("db_uid", "root"); // change this to yours
+// define("db_pwd", ""); // change this to yours (I won't show mine)
+// define("db_name", "cs2102"); // this is the one that we created earlier in this lecture
+
+
+define("db_host", "128.199.185.8");
 define("db_uid", "root"); // change this to yours
-define("db_pwd", ""); // change this to yours (I won't show mine)
+define("db_pwd", "pass"); // change this to yours (I won't show mine)
 define("db_name", "cs2102"); // this is the one that we created earlier in this lecture
+
+
 
 $db = new mysqli(db_host, db_uid, db_pwd, db_name);
 if ($db->connect_errno) { // are we connected properly?
