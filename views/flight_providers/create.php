2941583c4b8e9fb9bca61e16b0cbf2516642dae0
Viraj <virajhk@gmail.com>
2015-03-28 17:17:55 +0800

    Add new airline to database

--- views/flight_providers/create.php
+++ views/flight_providers/create.php
@@ -18,23 +18,23 @@
 			</div>
 			<div class="row">
 				<div class="col-xs-12">
-					<form action="" class="form-horizontal">
+					<form action="new_flight_provider.php" method="POST" class="form-horizontal">
 						<div class="form-group">
 							<label for="airlinecode-container" class="col-sm-6 col-xs-12 control-label">IATA 2-Character Code</label>
 							<div class="col-sm-6 col-xs-12">
-								<input type="text" class="form-control" id="airlinecode-container" placeholder="2X">
+								<input type="text" name="code" class="form-control" id="airlinecode-container" placeholder="2X">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="name-container" class="col-sm-6 col-xs-12 control-label">Official Name of the Airline</label>
 							<div class="col-sm-6 col-xs-12">
-								<input type="text" class="form-control" id="name-container" placeholder="Singapore Airlines">
+								<input type="text" name="name" class="form-control" id="name-container" placeholder="Singapore Airlines">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="logo-container" class="col-sm-6 col-xs-12 control-label">URL to Airline Logo</label>
 							<div class="col-sm-6 col-xs-12">
-								<input type="text" class="form-control" id="logo-container" placeholder="http://img2.wikia.nocookie.net/__cb20100506081728/logopedia/images/f/fa/Singapore_Airlines.svg">
+								<input type="text" name="url" class="form-control" id="logo-container" placeholder="http://img2.wikia.nocookie.net/__cb20100506081728/logopedia/images/f/fa/Singapore_Airlines.svg">
 							</div>
 						</div>
 						<div class="form-group">
