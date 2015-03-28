83b646e759baf9b971ddc29fb7029ab3afd5cc24
Viraj <virajhk@gmail.com>
2015-03-28 18:10:37 +0800

    Deleted dummy listing

--- views/users.php
+++ views/users.php
@@ -13,24 +13,6 @@
   	<div class="row">
   		<div class="col-xs-12">
   			<ol class="list-group">
-  				<li class="list-group-item">
-  					<div class="row">
-  						<div class="col-xs-8">
-  						<h5>
-                <strong>Bob Everyman</strong>
-								<span>bob@example.com</span>
-  						</h5>
-  						</div>
-  						<div class="col-xs-4 text-right">
-  							<button class="btn btn-info">
-  								Make Admin
-  							</button>
-  							<button class="btn btn-danger">
-  								Delete
-  							</button>
-  						</div>
-  					</div>
-  				</li>
 
           <?php 
 
