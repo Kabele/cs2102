006a71d96783c70df25f591d6f733828d2c0833f
SavinVarshney <savin.varshney@gmail.com>
2015-04-04 15:46:01 +0800

    Register user - Login session
    After register user automatically gets logged in

--- views/users/new_user.php
+++ views/users/new_user.php
@@ -12,6 +12,20 @@ require_once ($path . 'php/connect.php');
 $sql = "INSERT into website_user Values('".$email."', '".$password."', '".$authentication."', '".$admin."')";
 $result = $db->query($sql);
 
-header ('Location: ' . '/index.php');
 
+
+///header ('Location: ' . '/auth/login.php?ac=log&email=' . $email . '&password=' .$password); 
 ?>
+
+<html>
+	<form name='redirect' action='/auth/login.php' method='POST'>
+		<input type='hidden' name='ac' value='log'>
+		<input type='hidden' name='email' value='<?php echo $email; ?>'>
+		<input type='hidden' name='password' value='<?php echo $password; ?>'>
+		<input type='submit' value='Proceed'>
+	</form>
+
+<script type='text/javascript'>
+document.redirect.submit();
+</script>
+</html>
