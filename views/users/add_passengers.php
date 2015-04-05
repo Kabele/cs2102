36d2e50b52f308b6a0116c24275be3591f64fc5e
Viraj <virajhk@gmail.com>
2015-04-05 22:38:32 +0800

    Add and view passengers for each person

--- views/users/add_passengers.php
+++ views/users/add_passengers.php
@@ -5,6 +5,7 @@ include( $path . "views/partials/global.php" );
 
 $firstname = $_POST["passenger-firstname"];
 $lastname = $_POST["passenger-lastname"];
+$self = $_POST["self"];
 $email = $_SESSION["logged"];
 $diet = $_POST["diet"];
 $passport = $_POST["passenger-passport"];
@@ -13,7 +14,7 @@ $phone = $_POST["passenger-phone"];
 
 require_once ($path . 'php/connect.php');
 
-$sql = "INSERT into passenger Values('".$firstname."', '".$lastname."', '".$email."', '".$phone."', '".$passport."', '".$country."', '".$diet."')";
+$sql = "INSERT into passenger Values('".$firstname."', '".$lastname."', '".$self."','".$email."', '".$phone."', '".$passport."', '".$country."', '".$diet."');";
 $result = $db->query($sql);
 
 header ('Location: ' . '/views/users/passengers.php');
--- views/users/passengers.php
+++ views/users/passengers.php
@@ -29,12 +29,13 @@
 							</div>
 							<div class="form-group">
 								<input type="text" class="form-control" name="passenger-lastname" id="passenger-lastname" placeholder="Last Name">
+								<input type="hidden" class="form-control" name="self" id="passenger-lastname" value="0">
 							</div>
 							<div class="form-group">
 								<select id="diet" name="diet" class="form-control">
-									<option value="veg">Vegetarian</option>
-									<option value="nonveg">Non-Vegetarian</option>
-									<option value="halal">Halal</option>
+									<option value="Vegetarian">Vegetarian</option>
+									<option value="Non-Vegetarian">Non-Vegetarian</option>
+									<option value="Halal">Halal</option>
 								</select>
 							</div>
 							</p>
@@ -57,19 +58,50 @@
 					</div>
 				</div>
 				<br>
-				<ul class="list-group">
-					<li class="list-group-item">
-						<div class="row">
-							<div class="col-xs-10">
-								Ross Everyman (H94235490), United States
-								<span class="badge">Vegetarian</span>
-							</div>
-							<div class="col-xs-2">
-								<a href="#" class="btn btn-danger btn-sm pull-right">Remove</a>
-							</div>
-						</div>
-					</li>
-				</ul>
+				<ol class="list-group">
+					<?php 
+
+	        require_once $path.'/php/connect.php';
+
+	        $email = $_SESSION["logged"];
+
+	        $sql = "SELECT p.first_name, p.last_name, p.self, p.phone, p.passport, p.country, p.diet
+	            FROM passenger p WHERE p.user_email ='".$email."';";
+
+	          $res = $db->query($sql);
+
+	          while ($row = mysqli_fetch_assoc($res)) {
+
+	              echo '<li class="list-group-item">';
+	            echo '<div class="row">';
+	              echo '<div class="col-xs-12">';
+	              echo '<h5 style="display: inline;">';
+	                echo '<strong>'.$row['first_name'].'</strong>&nbsp';
+	                echo '<strong>'.$row['last_name'].'</strong>&nbsp';
+	                echo '(<u>Passport</u>: '.$row['passport'].', <u>Phone</u>: '.$row['phone'].'), ';
+	                echo $row['country'];
+	                echo ' &nbsp<span class="badge">'.$row['diet'].'</span>';
+	                echo '</h5>';
+	                echo '<form style="display: inline" action="remove_passenger.php" method="POST">';
+		              echo '<input type="hidden" name="email" value="'.$email.'">';
+		              echo '<input type="hidden" name="self" value="'.$row['self'].'">';
+		              echo '<button class="btn btn-danger btn-sm pull-right" type="submit" name="passport" value="'.$row['passport'].'">';
+		                echo 'Remove';
+		              echo '</button>';
+		            echo '</form>';
+	              echo '</div>';
+	          echo '</div>';
+	        echo '</li>';
+
+
+	          }
+
+
+
+
+
+	        ?>
+				</ol>
 			</div>
 		</div>
 	</div>
