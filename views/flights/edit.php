349775c2d2dbee6738b5575631350741396c648f
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-04-05 14:31:51 +0800

    Updating flight table

--- views/flights/edit.php
+++ views/flights/edit.php
@@ -1,26 +1,120 @@
-<?php
+<?php $path = $_SERVER['DOCUMENT_ROOT'] . "/"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Discount Airlines - Edit Flight</title>
+	<?php include( $path . "views/partials/meta.php" ); ?>
+	<?php include( $path . "views/partials/styles.php" ); ?>
+	<?php include( $path . "views/partials/scripts.php" ); ?>
+</head>
+<body>
+	<?php include( $path . "views/partials/navbar.php" ); 
+	require_once $path.'php/connect.php'; 
 
-// connect and set option
-$path = $_SERVER['DOCUMENT_ROOT'] . "/";
+	$flight_id = $_POST["flight_id"];
+	$departure_date = $_POST["departure_date"];
 
-require_once $path.'php/connect.php';
 
-$flight_id = $_POST["flight_id"];
-$departure = $_POST["departure"];
-$arrival = $_POST["arrival"];
-$departure_date = $_POST["departure_date"];
-$arrival_date = $_POST["arrival_date"];
-$passenger_limit = $_POST["passenger_limit"];
-$status = $_POST["status"];
-$status_changed_by = $_POST["status_changed_by"];
-$price = $_POST["price"];
-$airline_code = $_POST["airline_code"];
+	$sql = "SELECT * FROM flight WHERE flight_id = '".$flight_id."' AND departure_date = '". $departure_date."';";
 
-$sql = "UPDATE flight SET departure = '".$departure."', arrival = '".$arrival."', departure_date = '".$departure_date."', arrival_date = '".$arrival_date."', passenger_limit = '".$passenger_limit."', status = '".$status."', status_changed_by = '".
-	$status_changed_by."', price = '".$price."', airline_code = '".$airline_code."' WHERE flight_id = '".$flight_id."' AND departure_date = '". $departure_date."';";
+	$res = $db->query($sql);
+	$row = mysqli_fetch_assoc($res);
 
-$db->query($sql);
 
-?>
+	?>
+	<div class="container">
+		<div class="well">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1 class="text-center">Edit Flight</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12">
+					<form action="update.php" method="post" class="form-horizontal">
+						<div class="form-group">
+							<label for="flightid-container" class="col-sm-6 col-xs-12 control-label">
+							Flight Code
+							</label>
+							<div class="col-sm-6 col-xs-12">
+								<input type="text" name="flight_id" class="form-control" id="flightid-container" placeholder="<?php echo $row['flight_id'];?>">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="depart-container" class="col-sm-6 col-xs-12 control-label">
+							Departing Airport
+							</label>
+							<div class="col-sm-6 col-xs-12">
+								<input type="text" name="departure" class="form-control" id="depart-container" placeholder="<?php echo $row['departure'];?>">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="departtime-container" class="col-sm-6 col-xs-12 control-label">
+							Departing Date/Time
+							</label>
+							<div class="col-sm-6 col-xs-12">
+								<input type="datetime-local" name="departure_date" class="form-control" id="<?php echo $row['departure_date'];?>">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="arrive-container" class="col-sm-6 col-xs-12 control-label">
+							Arriving Airport
+							</label>
+							<div class="col-sm-6 col-xs-12">
+								<input type="text" class="form-control" name="arrival" id="arrive-container" placeholder="<?php echo $row['arrival'];?>">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="arrivetime-container" class="col-sm-6 col-xs-12 control-label">
+							Arriving Date/Time
+							</label>
+							<div class="col-sm-6 col-xs-12">
+								<input type="datetime-local" name="arrival_date" class="form-control" id="arrivetime-container" placeholder="<?php echo $row['arrival_date'];?>">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="passlimit-container" class="col-sm-6 col-xs-12 control-label">
+							Total number of passengers
+							</label>
+							<div class="col-sm-6 col-xs-12">
+								<input type="number" class="form-control" name="passenger_limit" id="passlimit-container" min="1" placeholder="<?php echo $row['passenger_limit'];?>">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="price-container" class="col-sm-6 col-xs-12 control-label">
+							Price per Seat
+							</label>
+							<div class="col-sm-6 col-xs-12">
+								<input type="number" class="form-control" name="price" id="price-container" min="0" placeholder="<?php echo $row['price'];?>">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="airline-container" class="col-sm-6 col-xs-12 control-label">
+							Operating Airlines	
+							</label>
+							<div class="col-sm-6 col-xs-12">
+								<select name="airline_code" id="" class="form-control" >
+									<?php 
+									$sql = "SELECT a.airline_code, a.name FROM airline a;";
 
-<?php include( $path . "views/flights.php" ); ?>      
+          							$res = $db->query($sql);
+
+          							while ($row = mysqli_fetch_assoc($res)) { 
+          								echo '<option value="'.$row['airline_code'].'" placeholder="'.$row['airline_code'].'">'.$row['name'].'</option>';
+          							}
+
+
+          							?>
+								</select>
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-6 col-sm-offset-6 col-xs-12">
+								<button type="submit" class="btn btn-primary">Edit Flight</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
---
+++ views/flights/update.php
@@ -0,0 +1,26 @@
+<?php
+
+// connect and set option
+$path = $_SERVER['DOCUMENT_ROOT'] . "/";
+
+require_once $path.'php/connect.php';
+
+$flight_id = $_POST["flight_id"];
+$departure = $_POST["departure"];
+$arrival = $_POST["arrival"];
+$departure_date = $_POST["departure_date"];
+$arrival_date = $_POST["arrival_date"];
+$passenger_limit = $_POST["passenger_limit"];
+$status = $_POST["status"];
+$status_changed_by = $_POST["status_changed_by"];
+$price = $_POST["price"];
+$airline_code = $_POST["airline_code"];
+
+$sql = "UPDATE flight SET departure = '".$departure."', arrival = '".$arrival."', departure_date = '".$departure_date."', arrival_date = '".$arrival_date."', passenger_limit = '".$passenger_limit."', status = '".$status."', status_changed_by = '".
+	$status_changed_by."', price = '".$price."', airline_code = '".$airline_code."' WHERE flight_id = '".$flight_id."' AND departure_date = '". $departure_date."';";
+
+$db->query($sql);
+
+?>
+
+<?php include( $path . "views/flights.php" ); ?>      
