9f272c60f120dbf3b9197ff17475d3f82da09628
SavinVarshney <savin.varshney@gmail.com>
2015-04-05 16:52:58 +0800

    register user redirect

--- views/users/new_user.php
+++ views/users/new_user.php
@@ -12,14 +12,19 @@ require_once ($path . 'php/connect.php');
 $sql = "INSERT into website_user Values('".$email."', '".$password."', '".$authentication."', '".$admin."')";
 $result = $db->query($sql);
 
-
+if(!$result){
+	$log = "notlog";
+	header('Location:' . '/views/users/create.php');
+}else{
+	$log = "log";
+}
 
 ///header ('Location: ' . '/auth/login.php?ac=log&email=' . $email . '&password=' .$password); 
 ?>
 
 <html>
 	<form name='redirect' action='/auth/login.php' method='POST'>
-		<input type='hidden' name='ac' value='log'>
+		<input type='hidden' name='ac' value=<?php echo $log?>>
 		<input type='hidden' name='email' value='<?php echo $email; ?>'>
 		<input type='hidden' name='password' value='<?php echo $password; ?>'>
 		<input type='submit' value='Proceed'>
