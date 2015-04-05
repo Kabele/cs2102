9bac54d378fa0946535ee41d4dc38e3ef9c7f043
Allwin Panikulam <allwinpb@gmail.com>
2015-04-05 15:12:42 +0800

    create and return-flight combined to one

--- views/bookings/create.php
+++ views/bookings/create.php
@@ -11,7 +11,38 @@
 <body>
 	<?php include( $path . "views/partials/navbar.php" ); ?>
 	<div class="container">
-		<h1 class="text-center">Choose Your Flight</h1>
+		<h1 class="text-center">Choose Your Forward Flight</h1>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="list-group">
+					<a href="return-flight.php" class="list-group-item">
+						<h1>
+							S$830<small>/<span class="glyphicon glyphicon-user" aria-hidden="true"></span></small>
+							<img src="http://img2.wikia.nocookie.net/__cb20100506081728/logopedia/images/f/fa/Singapore_Airlines.svg" alt="" class="logo">
+							<small>
+								Singapore Airlines
+							</small>
+						</h1>
+						<p>
+							Departing from 
+							<span class="lead">Changi International Airport</span>
+							on 
+							<span class="lead">Saturday, 14th March 2015</span>
+							at 
+							<span class="lead">8:30 AM</span>
+							<br>
+							and expected to reach 
+							<span class="lead">Dubai International Airport</span>
+							on 
+							<span class="lead">the same day</span>
+							at 
+							<span class="lead">6:00 PM</span> local time.
+						</p>
+					</a>
+				</div>
+			</div>
+		</div>
+		<h1 class="text-center">Choose Your Return Flight</h1>
 		<div class="row">
 			<div class="col-xs-12">
 				<div class="list-group">
