4bd535d723d7573f3a75c4af9a0b7c204a6f196e
Viraj <virajhk@gmail.com>
2015-04-05 18:38:31 +0800

    Add new passengers

---
+++ views/users/add_passengers.php
@@ -0,0 +1,20 @@
+<?php
+
+$path = $_SERVER['DOCUMENT_ROOT'] . "/";
+include( $path . "views/partials/global.php" );
+
+$firstname = $_POST["passenger-firstname"];
+$lastname = $_POST["passenger-lastname"];
+$email = $_SESSION["logged"];
+$diet = $_POST["diet"];
+$passport = $_POST["passenger-passport"];
+$country = $_POST["passenger-country"];
+$phone = $_POST["passenger-phone"];
+
+require_once ($path . 'php/connect.php');
+
+$sql = "INSERT into passenger Values('".$firstname."', '".$lastname."', '".$email."', '".$phone."', '".$passport."', '".$country."', '".$diet."')";
+$result = $db->query($sql);
+
+header ('Location: ' . '/views/users/passengers.php');
+?>
--- views/users/passengers.php
+++ views/users/passengers.php
@@ -22,16 +22,16 @@
 			<div class="col-sm-8 col-xs-12">
 				<div class="row">
 					<div class="col-xs-12">
-						<form action="" class="form-inline">
+						<form action="add_passengers.php" class="form-inline" method="POST">
 							<p>
 							<div class="form-group">
-								<input type="text" class="form-control" id="passenger-firstname" placeholder="First Name">
+								<input type="text" class="form-control" name="passenger-firstname" id="passenger-firstname" placeholder="First Name">
 							</div>
 							<div class="form-group">
-								<input type="text" class="form-control" id="passenger-lastname" placeholder="Last Name">
+								<input type="text" class="form-control" name="passenger-lastname" id="passenger-lastname" placeholder="Last Name">
 							</div>
 							<div class="form-group">
-								<select name="" id="" class="form-control">
+								<select id="diet" name="diet" class="form-control">
 									<option value="veg">Vegetarian</option>
 									<option value="nonveg">Non-Vegetarian</option>
 									<option value="halal">Halal</option>
@@ -40,18 +40,23 @@
 							</p>
 							<p>
 							<div class="form-group">
-								<input type="text" class="form-control" id="passenger-passport" placeholder="Passport">
+								<input type="text" class="form-control" name="passenger-passport" id="passenger-passport" placeholder="Passport">
 							</div>
 							<div class="form-group">
-								<input type="text" class="form-control" id="passenger-country" placeholder="Country">
+								<input type="text" class="form-control" name="passenger-country" id="passenger-country" placeholder="Country">
 							</div>
 							<div class="form-group">
-								<button class="btn btn-default">Add New Passenger</button>
+								<input type="text" class="form-control" name="passenger-phone" id="passenger-phone" placeholder="Phone Number">
 							</div>
+							<br>
 							</p>
+							<div class="form-group">
+								<button type="submit" class="btn btn-default">Add New Passenger</button>
+							</div>
 						</form>
 					</div>
 				</div>
+				<br>
 				<ul class="list-group">
 					<li class="list-group-item">
 						<div class="row">
